a0eaf7ce0d6084ddffb568c00c8780445d704c3d
Gaston82 <cabezaartaza@hotmail.com>
2023-02-27 17:17:08 +0100

    Install button font

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./components/Button/Button";
 
 const App = (): JSX.Element => {
-  return <Button text={"Send"} />;
+  return <Button text={"Sign up"} />;
 };
 
 export default App;
