987072c29aad3381a6fa46aba5e3a377690dda7b
Gaston82 <cabezaartaza@hotmail.com>
2023-02-27 16:52:05 +0100

    Create ButtonStyled component

---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  background-color: #cccf18;
+  width: 323px;
+  height: 40px;
+  border-radius: 10px;
+  font-size: 20px;
+  color: #1e1e1e;
+  font-family: "Saira Stencil One";
+  font-weight: 400;
+`;
+
+export default ButtonStyled;
